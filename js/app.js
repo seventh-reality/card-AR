45bb28901367022fb6ff26124b960d600813e054
seventhreality <104563775+seventh-reality@users.noreply.github.com>
2023-08-17 15:57:14 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/whe_31.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/whe_32.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
